d47904491fb7c0b55be5c86b978897b3201ecbc0
iscander <iscander161@yandex.ru>
2023-01-10 22:49:46 +0300

    Добавлена домашняя работа № 2

---
+++ homework/move_me_out.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file move_me_out.h
+ * @author Alexander Tolstunov (iscander161@yandex.ru)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include "move_me_out.h"
+
+/*
+ * Move Me Out function
+ */
+void todo_move_me_out() 
+{
+    std::cout << "Testing \"move me out\" function from other location..." << std::endl;
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ homework/move_me_out.h
@@ -0,0 +1,21 @@
+/**
+ * @file move_me_out.h
+ * @author Alexander Tolstunov (iscander161@yandex.ru)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _MOVE_ME_OUT_H_
+#define _MOVE_ME_OUT_H_
+
+/**
+ * @brief Move Me Out function
+ */
+void todo_move_me_out();
+
+#endif /* _MOVE_ME_OUT_H_ */
