b02ec668504a019e9f1c0ba354539a9738c2f59c
iscander <iscander161@yandex.ru>
2023-01-23 21:00:57 +0300

    Forgot the main file

---
+++ 03_homework/main.c
@@ -0,0 +1,26 @@
+/**
+ * @file main.c
+ * @author Alexander Tolstunov (iscander161@yandex.ru)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-23
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include <iostream>
+#include "main.h"
+
+/**
+ * @brief Main function of application
+ * @param argc Arguments 
+ * @param argvs Arguments of application
+ * @return int Result of application
+ * @retval !0 - Some error occured
+ */
+int main(int argc, char** argvs)
+{
+    return EXIT_SUCCESS;
+}
